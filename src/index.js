0182d15104f4487b2bc2f89fdf91dc940d34eb9f
nqhung291 <nqhung291@gmail.com>
2023-07-28 07:38:09 +0700

    feat: enable eslint

--- src/index.js
+++ src/index.js
@@ -1,46 +1,44 @@
-import './style.css'
-import * as THREE from 'three'
+import './style.css';
+import * as THREE from 'three';
 
 const sizes = {
   width: 800,
-  height: 600
-}
+  height: 600,
+};
 
 const main = () => {
   // scene = camera + object inside
   // object (mesh) = geometry + material
-  const scene = new THREE.Scene()
-  
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1,1,1,5,5,5),
-    new THREE.MeshBasicMaterial({ color: 'red' })
-  )
-  scene.add(mesh)
+  const scene = new THREE.Scene();
 
-  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-  camera.position.x = 2
-  camera.position.y = 2
-  camera.position.z = 2
-  camera.lookAt(mesh.position)
-  scene.add(camera)
+  const mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1, 5, 5, 5), new THREE.MeshBasicMaterial({ color: 'red' }));
+  scene.add(mesh);
+
+  // const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+  const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100);
+  camera.position.x = 2;
+  camera.position.y = 2;
+  camera.position.z = 2;
+  camera.lookAt(mesh.position);
+  scene.add(camera);
 
   // need a renderer to render scene to html
   const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('.webgl')
-  })
-  renderer.setSize(sizes.width, sizes.height)
+    canvas: document.querySelector('.webgl'),
+  });
+  renderer.setSize(sizes.width, sizes.height);
 
-  const clock = new THREE.Clock()
+  const clock = new THREE.Clock();
 
   const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
-    mesh.rotation.x = elapsedTime
-    mesh.rotation.y = elapsedTime
-    mesh.rotation.z = elapsedTime
+    const elapsedTime = clock.getElapsedTime();
+    mesh.rotation.x = elapsedTime;
+    mesh.rotation.y = elapsedTime;
+    mesh.rotation.z = elapsedTime;
     renderer.render(scene, camera);
-    window.requestAnimationFrame(tick)
-  }
-  tick()
-}
+    window.requestAnimationFrame(tick);
+  };
+  tick();
+};
 
-main()
+main();
