dbfc2f1e25d2c3336d84bb6f7eb52dc17b71552d
nqhung291 <nqhung291@gmail.com>
2023-07-28 09:39:52 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import './style.css';
 import * as THREE from 'three';
 
 const sizes = { width: 800, height: 600 };
+const cursor = {
+  x: 0,
+  y: 0,
+};
+const aspectRatio = sizes.width / sizes.height;
+
+window.addEventListener('mousemove', (e) => {
+  cursor.x = e.clientX / sizes.width - 0.5;
+  cursor.y = -(e.clientY / sizes.height - 0.5);
+});
 
 const main = () => {
   // scene = camera + object inside
@@ -14,12 +24,19 @@ const main = () => {
   );
   scene.add(mesh);
 
-  // const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-  const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100);
-  camera.position.x = 2;
-  camera.position.y = 2;
-  camera.position.z = 2;
-  camera.lookAt(mesh.position);
+  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+  // const camera = new THREE.OrthographicCamera(
+  //   -1 * aspectRatio,
+  //   1 * aspectRatio,
+  //   1,
+  //   -1,
+  //   0.1,
+  //   100,
+  // );
+  // camera.position.x = 2;
+  // camera.position.y = 2;
+  camera.position.z = 3;
+  // camera.lookAt(mesh.position);
   scene.add(camera);
 
   // need a renderer to render scene to html
@@ -28,13 +45,16 @@ const main = () => {
   });
   renderer.setSize(sizes.width, sizes.height);
 
-  const clock = new THREE.Clock();
+  // const clock = new THREE.Clock();
 
   const tick = () => {
-    const elapsedTime = clock.getElapsedTime();
-    mesh.rotation.x = elapsedTime;
-    mesh.rotation.y = elapsedTime;
-    mesh.rotation.z = elapsedTime;
+    // const elapsedTime = clock.getElapsedTime();
+    // mesh.rotation.x = elapsedTime;
+    // mesh.rotation.y = elapsedTime;
+    // mesh.rotation.z = elapsedTime;
+    camera.position.x = cursor.x * 3;
+    camera.position.y = cursor.y * 3;
+    // camera.lookAt(new THREE.Vector3());
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick);
   };
