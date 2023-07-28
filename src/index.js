9b1fbbd1e81d8ac1ac7e1972a8926489fa43cbd6
nqhung291 <nqhung291@gmail.com>
2023-07-28 16:15:24 +0700

    done lession 8

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import './style.css';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const sizes = { width: 800, height: 600 };
+const sizes = { width: window.innerWidth, height: window.innerHeight };
 const cursor = {
   x: 0,
   y: 0,
 };
-const aspectRatio = sizes.width / sizes.height;
-
-window.addEventListener('mousemove', (e) => {
-  cursor.x = e.clientX / sizes.width - 0.5;
-  cursor.y = -(e.clientY / sizes.height - 0.5);
-});
 
 const main = () => {
   // scene = camera + object inside
   // object (mesh) = geometry + material
+  const canvas = document.querySelector('.webgl');
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+  });
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
   const scene = new THREE.Scene();
 
   const mesh = new THREE.Mesh(
@@ -36,14 +38,24 @@ const main = () => {
   // camera.position.x = 2;
   // camera.position.y = 2;
   camera.position.z = 3;
-  // camera.lookAt(mesh.position);
   scene.add(camera);
+  // camera.lookAt(mesh.position);
 
-  // need a renderer to render scene to html
-  const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('.webgl'),
+  const control = new OrbitControls(camera, canvas);
+  control.enableDamping = true;
+
+  window.addEventListener('mousemove', (e) => {
+    cursor.x = e.clientX / sizes.width - 0.5;
+    cursor.y = -(e.clientY / sizes.height - 0.5);
   });
-  renderer.setSize(sizes.width, sizes.height);
+  window.addEventListener('resize', () => {
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  });
 
   // const clock = new THREE.Clock();
 
@@ -52,9 +64,10 @@ const main = () => {
     // mesh.rotation.x = elapsedTime;
     // mesh.rotation.y = elapsedTime;
     // mesh.rotation.z = elapsedTime;
-    camera.position.x = cursor.x * 3;
-    camera.position.y = cursor.y * 3;
+    // camera.position.x = cursor.x * 3;
+    // camera.position.y = cursor.y * 3;
     // camera.lookAt(new THREE.Vector3());
+    control.update();
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick);
   };
