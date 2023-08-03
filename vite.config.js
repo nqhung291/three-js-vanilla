7d2af8a5eb196bf41370a8330b30dbde9b37dce7
nqhung291 <nqhung291@gmail.com>
2023-08-04 00:35:13 +0700

    test manual chunks

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 /** @type {import('vite').UserConfig} */
 export default {
-  root: "src/",
-  base: "./",
+  root: 'src/',
+  base: './',
   publicDir: '../public/',
   server: {
-    host: true
-  }
-}
+    host: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['three', 'lil-gui'],
+          lib: ['gsap'],
+        },
+      },
+    },
+  },
+};
