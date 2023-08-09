551466231b76a8bbfdd4dc95ee43f9617858e894
nqhung291 <nqhung291@gmail.com>
2023-08-09 17:44:09 +0700

    config build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,33 @@
-/** @type {import('vite').UserConfig} */
-export default {
-  root: 'src/',
-  base: './',
+import path from 'path';
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+
+const root = path.resolve(__dirname, 'src');
+const outDir = path.resolve(__dirname, 'dist');
+
+const input = Object.fromEntries(
+  glob.sync('src/**/index.html').map((file) => {
+    const key = path.relative('src', file).replace('/index.html', '');
+    return [key, path.resolve(__dirname, file)];
+  }),
+);
+
+export default defineConfig({
+  root,
   publicDir: '../public/',
   server: {
     host: true,
   },
+  resolve: {
+    alias: {
+      '@': root,
+    },
+  },
   build: {
+    outDir,
+    emptyOutDir: true,
     rollupOptions: {
+      input,
       output: {
         manualChunks: {
           vendor: ['three', 'lil-gui'],
@@ -16,4 +36,4 @@ export default {
       },
     },
   },
-};
+});
